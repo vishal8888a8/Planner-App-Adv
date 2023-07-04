b4c3431e027452c6d1c28a9dd6969320bb786980
vishal <vishalgiri8507012571@gmail.com>
2023-07-04 12:22:08 +0530

    First prototype

---
+++ src/songsList.js
@@ -0,0 +1,30 @@
+export const songsList = [
+  {
+    id: 0,
+    title: "Summer Walk",
+    artist: "Olexy",
+    img_src: "./songs/songsImages/img1.jpg",
+    src: "./songs/summer walk.mp3",
+  },
+  {
+    id: 1,
+    title: "Leva - Eternity",
+    artist: "lemonmusicstudio",
+    img_src: "./songs/songsImages/img2.jpg",
+    src: "./songs/leva eternity.mp3",
+  },
+  {
+    id: 2,
+    title: "Meditative background music",
+    artist: "DMD_Production",
+    img_src: "./songs/songsImages/img3.jpg",
+    src: "./songs/meditative background music.mp3",
+  },
+  {
+    id: 3,
+    title: "background light meditative.mp3",
+    artist: "DMD_Production",
+    img_src: "./songs/songsImages/img4.jpg",
+    src: "./songs/background light meditative.mp3",
+  },
+];
---
+++ src/widgets/MyCustomWidget.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
+import "../styles/MyCustomWidget.css";
+
+//JSON data of songsList
+import { songsList } from "../songsList";
+
+export default function MyCustomWidget() {
+  const [currentSong, setCurrentSong] = useState(songsList[0]);
+
+  const playNextSong = () => {
+    setCurrentSong(
+      (prevSong) => songsList[(prevSong.id + 1) % songsList.length]
+    );
+  };
+
+  const playPreviousSong = () => {
+    setCurrentSong(
+      (prevSong) =>
+        songsList[(prevSong.id - 1 + songsList.length) % songsList.length]
+    );
+  };
+
+  return (
+    <div className="MusicPlayer">
+      <h2>Currently Playing</h2>
+      <img
+        src={currentSong.img_src}
+        alt="Artist pic"
+        className="Thumbnail"
+        draggable={false}
+      />
+      <div style={{ textAlign: "center" }}>
+        <h3>{currentSong.title}</h3>
+        <h5>{currentSong.artist}</h5>
+      </div>
+      <div style={{ width: "17rem" }}>
+        <AudioPlayer
+          src={currentSong.src}
+          showJumpControls={false}
+          showSkipControls={true}
+          showFilledVolume={false}
+          onClickNext={() => playNextSong()}
+          onClickPrevious={() => playPreviousSong()}
+        />
+      </div>
+    </div>
+  );
+}
